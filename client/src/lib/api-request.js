ad60ffa08be5bb84a2a4d6baf07bb2e034df4dfa
Mora Frederic <frederic.mora@unilim.fr>
2024-10-16 11:56:11 +0200

    update api-request.js to handler POST request with FormData

--- client/src/lib/api-request.js
+++ client/src/lib/api-request.js
@@ -36,7 +36,7 @@ let getRequest = async function(uri){
         return false;
     }
     if (response.status != 200){
-        console.error("Erreur de requête : "+response.status); // affichage de l'erreur dans la console
+        console.error("Erreur de requête : " + response.status); // affichage de l'erreur dans la console
         return false; // si le serveur a renvoyé une erreur, on retourne false
     }  // si le serveur a renvoyé une erreur, on retourne false
     let $obj = await response.json(); // extraction du json retourné par le serveur (opération asynchrone aussi)
@@ -54,22 +54,27 @@ let getRequest = async function(uri){
  * 
  *  Le serveur retourne en JSON la nouvelle ressource créée en base avec son identifiant.
  *  La fonction retourne les données après conversion en objet Javascript (ou false si la requête a échoué)
- * 
- *  ATTENTION : La fonction est asynchrone, donc quand on l'appelle il ne faut pas oublier "await".
- *  Exemple : let data = await postRequest(http://.../api/products, {name:"Pain", category:2});
  */
 let postRequest = async function(uri, data){
-    // encodage des données au format JSON (à vous de bien transmettre ce que le serveur attend)
-    let json = JSON.stringify(data);
+    
 
     // Défition des options de la requêtes
     let options = {
         method: 'POST',
-        headers: {'Content-type': 'application/json;charset=utf-8'}, // on précise que la requête contient du json
-        body: json // le json est placé dans le corps de la requête
+        body: data
     }
 
-    let response = await fetch(API_URL+uri, options); // exécution (asynchrone) de la requête et attente de la réponse
+    try{
+        var response = await fetch(API_URL+uri, options); // exécution (asynchrone) de la requête et attente de la réponse
+    }
+    catch(e){
+        console.error("Echec de la requête : "+e); // affichage de l'erreur dans la console
+        return false;
+    }
+    if (response.status != 200){
+        console.error("Erreur de requête : " + response.status); // affichage de l'erreur dans la console
+        return false; // si le serveur a renvoyé une erreur, on retourne false
+    }
     let $obj = await response.json(); // extraction du json retourné par le serveur (opération asynchrone aussi)
     return $obj; // et on retourne le tout (response.json() a déjà converti le json en objet Javscript)
 }
